93189f45d594369d70b0c6ee26fca42cb5afe294
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2021-02-05 16:44:24 +0200

    Implementing Global Error Handling

--- server/controllers/PatientControllers.js
+++ server/controllers/PatientControllers.js
@@ -1,11 +1,24 @@
 import patientInfos from "../models/Patient";
+import catchAsync from "../utils/catchAsync";
+import AppError from "../utils/appError";
 
-export const createPatient = async (req,res)=>{
+export const createPatient = async (req, res) => {
+  const newPatient = await patientInfos.create(req.body);
 
-    const newPatient = await patientInfos.create(req.body);
+  res.status(201).json({
+    status: "success",
+    newPatient,
+  });
+};
 
-    res.status(201).json({
-        status:"success",
-        newPatient
-    })
-}
+export const getPatient = catchAsync(async (req, res, next) => {
+  const patient = await patientInfos.findById(req.params.id);
+  if (!patient)
+    return next(new AppError(404, "No patient found with that ID."));
+  res.status(200).json({
+    status: "success",
+    data: {
+      patient,
+    },
+  });
+});
--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import patientRouter from "./routes/PatientRoute";
+import globalErrorHandler from "./controllers/errorHandlerController";
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).send({
+    status: 200,
+    message: "welcome to patiente registration",
+  });
+});
 
-app.get('/', (req,res)=>{res.status(200).send({
-    status:200,
-    message:'welcome to patiente registration',
-})})
+app.use("/api/v1/patient", patientRouter);
 
-app.use("/api/v1/patient",patientRouter)
-
+app.use(globalErrorHandler);
 
 export default app;
